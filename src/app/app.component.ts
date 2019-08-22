7ac6a2036b6ed676c2f09b9ea12c972d80e325cf
Pedro Cardoso da Silva <forsureitsme@gmail.com>
2019-08-22 16:36:50 -0300

    Fix app entrypoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
+import { Component } from '@angular/core';
+import { AmplifyService } from 'aws-amplify-angular';
 import { APIService } from '../API';
 import { Restaurant } from './types/restaurant';
 
 @Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
   template: `
     <div>
       <div *ngFor="let restaurant of restaurants">
@@ -9,9 +14,16 @@ import { Restaurant } from './types/restaurant';
       </div>
     </div>`
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   restaurants: Array<Restaurant>;
-  constructor(public api: APIService) { }
+
+  constructor(
+    public api: APIService,
+    public amplify: AmplifyService,
+  ) {
+    amplify.auth().currentAuthenticatedUser().then(console.log)
+  }
+
   ngOnInit() {
     this.api.ListRestaurants().then(data => {
       this.restaurants = data.items;
